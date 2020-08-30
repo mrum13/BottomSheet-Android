ccb1ddf82f624eb1f657fb1bf4ddcf536f9ca476
mrum13 <muhammadrum135@gmail.com>
2020-08-30 16:55:11 +0800

    30 Agustus 2020

---
+++ app/src/main/java/com/example/bottomsheet/BottomSheetListener.java
@@ -0,0 +1,6 @@
+package com.example.bottomsheet;
+
+public interface BottomSheetListener {
+    void onButtonClicked(String text);
+
+}
---
+++ app/src/main/java/com/example/bottomsheet/ExampleBottomSheetDialog.java
@@ -0,0 +1,59 @@
+package com.example.bottomsheet;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class ExampleBottomSheetDialog extends BottomSheetDialogFragment {
+    private BottomSheetListener mListener;
+
+    public ExampleBottomSheetDialog() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.bottom_sheet_layout, container, false);
+
+        Button button1 = v.findViewById(R.id.button1);
+        Button button2 = v.findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onButtonClicked("Button 1 clicked");
+                dismiss();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onButtonClicked("Button 2 clicked");
+                dismiss();
+            }
+        });
+
+        return v;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        try {
+            mListener = (BottomSheetListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                    + " must implement BottomSheetListener");
+        }
+    }
+}
